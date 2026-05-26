b4113de37ff089398d7a52a473b88d92602066dc
prak-react <gibran24si@mahsiswa.pcr.ac.id>
2026-05-26 15:37:35 +0700

    shadcn component

--- src/pages/FiturXyz.jsx
+++ src/pages/FiturXyz.jsx
@@ -1,13 +1,54 @@
 import { FaShoppingCart, FaTruck, FaBan, FaDollarSign } from "react-icons/fa";
 import PageHeader from "../components/PageHeader";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 
 export default function FiturXyz() {
+  // 1️⃣ MODIFIKASI: Mengorganisir data ke dalam Array of Objects
 
-    return (
-        <div className="p-6 bg-gray-50 min-h-screen">
-            <PageHeader title="Fitur Xyz" />
+  return (
+    <div id="dashboard-container" className="p-4">
+      <PageHeader title="Fitur Xyz" />
+      <p>Ini adalah halaman untuk Fitur Xyz.</p>
 
-           
-        </div>
-    );
+      <Button variant="">Simpan</Button>
+      <Button variant="outline">Simpan</Button>
+      <Button variant="secondary">Simpan</Button>
+      <Button variant="ghost">Simpan</Button>
+      <Button variant="destructive">Simpan</Button>
+      <Button variant="link">Simpan</Button>
+
+      <Card className="mt-4 w-[380px]">
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle>Belajar shadcn/ui</CardTitle>
+            <Badge variant="secondary">Baru</Badge>
+          </div>
+          <CardDescription>
+            Contoh penggunaan komponen shadcn/ui di React
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <p className="text-sm text-muted-foreground">
+            Komponen ini dibuat di branch <strong>setup-shadcn</strong>
+            lalu di-merge ke main.
+          </p>
+        </CardContent>
+
+        <CardFooter className="flex gap-2">
+          <Button>Simpan</Button>
+          <Button variant="outline">Batal</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
+    react(),
     tailwindcss()
   ],
   resolve: {
@@ -13,4 +14,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  
 })
